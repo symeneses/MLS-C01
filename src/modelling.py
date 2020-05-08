e989827cf675f4a55d2fbd29272df8c0ef4ba73f
symeneses <sandrayojana@gmail.com>
2020-05-08 16:40:19 +0200

    Outleirs detection

--- src/modelling.py
+++ src/modelling.py
@@ -7,8 +7,10 @@ def plot_decision_function(X, y, clf, ax):
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
                          np.arange(y_min, y_max, plot_step))
-
-    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    try:
+        Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    except AttributeError:
+        Z = clf.fit_predict(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
     ax.contourf(xx, yy, Z, alpha=0.4)
     ax.scatter(X[:, 0], X[:, 1], alpha=0.8, c=y, edgecolor='k')
