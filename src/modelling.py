6fda75d6430857d78e96d273b7eafb4e9e9feb28
symeneses <sandrayojana@gmail.com>
2020-05-08 12:50:13 +0200

    Unbalanced dataasets

---
+++ src/modelling.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def plot_decision_function(X, y, clf, ax):
+    plot_step = 0.02
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
+                         np.arange(y_min, y_max, plot_step))
+
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    ax.contourf(xx, yy, Z, alpha=0.4)
+    ax.scatter(X[:, 0], X[:, 1], alpha=0.8, c=y, edgecolor='k')
